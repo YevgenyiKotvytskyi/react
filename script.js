e11e6ae39ec9b8eb28c008c5b79cd890f0db974c
Yevgenyi Kotvytskyi <yegeniy.kotvytskyi@gmail.com>
2020-08-21 13:13:34 +0300

    l01-es6 done

--- script.js
+++ script.js
@@ -1,43 +1,40 @@
-var employers = ['АртеМ', 'максим', 'Владимир', 'сергей', 'НикиТа', 'евГений', ' Дарья', ' ', 'виктория ', 'ЕкаТерина', '', ' Андрей ', 'КИРИЛЛ'];
-var nameCourse = 'Базовый React';
-var command = [];
-for (var i = 0; i < employers.length; i++) {
-	if (employers[i].length > 0 && employers[i].trim() != '') {
-		command.push(employers[i]);
-	}
-}
-for (var i = 0; i < command.length; i++) {
-	command[i] = command[i].toLowerCase().trim();
-	command[i] = command[i][0].toUpperCase() + command[i].slice(1);
-}
+'use strict';
 
-var data = {
-	cash: [3, 8, 3],
-	react: ['JSX', 'components', 'props', 'state', 'hooks'],
-	add: ['styled-components', 'firebase']
+const employers = ['АртеМ', 'максим', 'Владимир', 'сергей', 'НикиТа', 'евГений', ' Дарья',
+    ' ', 'виктория ', 'ЕкаТерина', '', ' Андрей ', 'КИРИЛЛ'];
+const nameCourse = 'Базовый React';
+
+const command = employers.filter(person => (person.length > 0 && person.trim() !== ''));
+
+command.forEach((person, index) => {
+    person = person.trim().toLowerCase();
+    command[index] = person[0].toUpperCase() + person.slice(1);
+});
+
+const data = {
+    cash: [3, 8, 3],
+    react: ['JSX', 'components', 'props', 'state', 'hooks'],
+    add: ['styled-components', 'firebase']
 };
 
-function calcCash(own) {
-	own = own || 0;
-	var everyCash = Array.prototype.slice.call(arguments);
-	var total = own;
-	for (var i = 0; i < everyCash[1].length; i++) {
-		total += +everyCash[1][i];
-	}
-	return total;
-}
 
-var lesson = calcCash(null, data.cash);
+const calcCash = (own = 0, ...arg) => {
+    let total = own;
+    const [data] = arg;
+    data.forEach(elem => total += +elem);
+    return total;
+};
 
-function makeBusiness(director, teacher, allModule, gang, course) {
-	teacher = teacher || 'Максим';
-	var sumTech = data.react.concat(data.add, 'и другие');
-	console.log('Стартуем новый курс: "' + course + '". Владелец: ' + director + ', преподаватель: ' + teacher + '. Всего уроков: ' + allModule + '. \nКоманда Академии: ' +
-		gang);
-	console.log('Первое что изучим будет ' + data.react[0] + ". Он очень похож на HTML!");
-	console.log('Технологии которые мы изучим: ');
-	console.log.apply(null, sumTech);
+const lesson = calcCash(null, data.cash);
 
-}
+const makeBusiness = (director, teacher, allModule, gang, course) => {
+    teacher = teacher || 'Максим';
+    console.log(
+        `Стартуем новый курс: "${course}.  Владелец: ${director}, преподаватель: ${teacher}. Всего уроков: ${allModule}
+Команда Академии: ${gang}`);
+    console.log('Первое что изучим будет ' + data.react[0] + ". Он очень похож на HTML!");
+    console.log('Технологии которые мы изучим: ');
+    console.log(...data.add, 'и другие');
+};
 
-makeBusiness.apply(null, ['Артем', null, lesson, command, nameCourse]);
+makeBusiness('Артем', null, lesson, command, nameCourse);
